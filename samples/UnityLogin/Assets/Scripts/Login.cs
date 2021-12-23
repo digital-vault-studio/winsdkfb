4612b5639f3901249f8dd855a1e70125e232248a
Benny Franco <benny.franco@digitalvaultstudio.com>
2021-12-22 19:46:23 -0700

    Add logout

--- samples/UnityLogin/Assets/Scripts/Login.cs
+++ samples/UnityLogin/Assets/Scripts/Login.cs
@@ -8,17 +8,23 @@ using winsdkfb;
 using winsdkfb.Graph;
 #endif
 
-public delegate void LogResult(string result);
+public delegate void LogResult(string result, string buttonText);
 
+[RequireComponent(typeof(Button))]
 public class Login : MonoBehaviour
 {
     [SerializeField]
     private Text facebookAccessToken;
 
+    private Button loginButton;
+    private Text loginButtonLabel;
+
     public static LogResult logger;
 
     private void Awake()
     {
+        loginButton = GetComponent<Button>();
+        loginButtonLabel = loginButton.GetComponentInChildren<Text>();
         logger = LogLoginResult;
     }
 
@@ -34,33 +40,42 @@ public class Login : MonoBehaviour
             };
 
             FBSession sess = FBSession.ActiveSession;
-            sess.FBAppId = "1025365401567256"; // AppID here
-            sess.WinAppId = "s-1-15-2-1638619580-1889396403-1308485934-3551780281-3637021990-998384026-3803739945"; // WinAppId here
 
-
-            FBPermissions permissions = new FBPermissions(permissionList);
-            FBResult result = await sess.LoginAsync(permissions, SessionLoginBehavior.WebAuth);
-
-            if (result.Succeeded)
+            if(sess.LoggedIn)
             {
-                // Login success                
-                Login.logger.Invoke(sess.AccessTokenData.AccessToken);
+                await sess.LogoutAsync();
+                Login.logger.Invoke("", "Login");
             }
             else
             {
-                // Login failed
-                Login.logger.Invoke(result.ErrorInfo.Code + " " + result.ErrorInfo.ErrorUserMessage);
+                sess.FBAppId = "1025365401567256"; // AppID here
+                sess.WinAppId = "s-1-15-2-1638619580-1889396403-1308485934-3551780281-3637021990-998384026-3803739945"; // WinAppId here
+
+                FBPermissions permissions = new FBPermissions(permissionList);
+                FBResult result = await sess.LoginAsync(permissions, SessionLoginBehavior.WebAuth);
+
+                if (result.Succeeded)
+                {
+                    // Login success                
+                    Login.logger.Invoke(sess.AccessTokenData.AccessToken, "Logout");
+                }
+                else
+                {
+                    // Login failed
+                    Login.logger.Invoke(result.ErrorInfo.Code + " " + result.ErrorInfo.ErrorUserMessage, "Login");
+                }
             }
         }, false);
 #endif
     }
 
-    public void LogLoginResult(string result)
+    public void LogLoginResult(string result, string buttonText)
     {
-#if ENABLE_WINMD_SUPPORT
+#if UNITY_WSA
         UnityEngine.WSA.Application.InvokeOnAppThread(() =>
         {
             facebookAccessToken.text = result;
+            loginButtonLabel.text = buttonText;
         }, false);
 #endif
     }
